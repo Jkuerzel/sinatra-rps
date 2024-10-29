96b4b9180dc8d528099dcac7e3e4a24c37621984
Jkuerzel <jkuerzel@chicagobooth.edu>
2024-10-29 01:46:55 +0000

    Final

--- app.rb
+++ app.rb
@@ -1,9 +1,39 @@
 require "sinatra"
 require "sinatra/reloader"
 
+rps=["rock","paper","scissors"]
+
+wlt_matrix = [
+  ["tied!", "lost!", "won!"],
+  ["won!", "tied!", "lost!"],
+  ["lost!", "won!", "tied!"]
+]
+
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:rule)
+end
+
+get("/rock") do
+  own_choise_i=0
+  opp_choise_i=rand(0..2)
+  @opp_choise=rps.at(opp_choise_i)
+  @wlt=wlt_matrix[own_choise_i][opp_choise_i]
+
+  erb(:rock)
+end
+
+get("/paper") do
+  own_choise_i=1
+  opp_choise_i=rand(0..2)
+  @opp_choise=rps.at(opp_choise_i)
+  @wlt=wlt_matrix[own_choise_i][opp_choise_i]
+  erb(:paper)
+end
+
+get("/scissors") do
+  own_choise_i=2
+  opp_choise_i=rand(0..2)
+  @opp_choise=rps.at(opp_choise_i)
+  @wlt=wlt_matrix[own_choise_i][opp_choise_i]
+  erb(:scissors)
 end
